9cd97ceb089d49fb74fd0e58205fab5afe14a5d9
Luka Bergant <luka@bergant.com>
2022-05-14 13:23:38 +0200

    Added serial.

--- RAK11300/DH_11300/src/main.cpp
+++ RAK11300/DH_11300/src/main.cpp
@@ -21,8 +21,34 @@ TinyGPS gps;
 
 void setup() {
   // put your setup code here, to run once:
+
+  // //Setup LEDs
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Initialize Serial for debug output
+  time_t timeout = millis();
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    if ((millis() - timeout) < 5000)
+    {
+      delay(100);
+    }
+    else
+    {
+      break;
+    }
+  }
 }
+
+int aliveCounter = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (aliveCounter++ % 100000 == 0)
+  {
+    Serial.print(aliveCounter / 100000);
+    Serial.println("Alive");
+  }
 }
