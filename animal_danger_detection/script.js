7f1e006143d8023f98d116375d16fb41f542eeaf
zigakerzan <ziga.kerzan@gmail.com>
2022-05-14 15:22:09 +0100

    First commit
    Created folders for map, road detection and animal danger

---
+++ animal_danger_detection/script.js
@@ -0,0 +1,128 @@
+// connect to html
+console.log("JavaScript is connected . . .")
+
+
+// find and connect to canvas
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+// load backgroud
+let bgImg = new Image();
+bgImg.src = './background.jpg';
+ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+
+
+// get random coordinates for the "road"
+let y1 = Math.floor(Math.random() * 200) + 200;
+let y2 = Math.floor(Math.random() * 200) + 200;
+
+// draw road
+function drawRoad() {
+    ctx.beginPath();
+    ctx.moveTo(0, y1);
+    ctx.lineTo(1000, y2);
+    ctx.stroke();
+}
+
+// circle to line collison detection
+function pointCircleCollide(point, circle, r) {
+    if (r===0) return false
+    var dx = circle[0] - point[0]
+    var dy = circle[1] - point[1]
+    return dx * dx + dy * dy <= r * r
+}
+
+var tmp = [0, 0]
+
+function lineCircleCollide(a, b, circle, radius, nearest) {
+    //check to see if start or end points lie within circle
+    if (pointCircleCollide(a, circle, radius)) {
+        if (nearest) {
+            nearest[0] = a[0]
+            nearest[1] = a[1]
+        }
+        return true
+    } if (pointCircleCollide(b, circle, radius)) {
+        if (nearest) {
+            nearest[0] = b[0]
+            nearest[1] = b[1]
+        }
+        return true
+    }
+
+    var x1 = a[0],
+        y1 = a[1],
+        x2 = b[0],
+        y2 = b[1],
+        cx = circle[0],
+        cy = circle[1]
+
+    //vector d
+    var dx = x2 - x1
+    var dy = y2 - y1
+
+    //vector lc
+    var lcx = cx - x1
+    var lcy = cy - y1
+
+    //project lc onto d, resulting in vector p
+    var dLen2 = dx * dx + dy * dy //len2 of d
+    var px = dx
+    var py = dy
+    if (dLen2 > 0) {
+        var dp = (lcx * dx + lcy * dy) / dLen2
+        px *= dp
+        py *= dp
+    }
+
+    if (!nearest)
+        nearest = tmp
+    nearest[0] = x1 + px
+    nearest[1] = y1 + py
+
+    //len2 of p
+    var pLen2 = px * px + py * py
+
+    //check collision
+    return pointCircleCollide(nearest, circle, radius)
+            && pLen2 <= dLen2 && (px * dx + py * dy) >= 0
+}
+
+// mouse move
+function mousemove(event){
+    ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+    drawRoad()
+
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    // outer circle
+    var hit1 = lineCircleCollide([0, y1], [1000, y2], [x, y], 25);
+    // inner circle
+    var hit2 = lineCircleCollide([0, y1], [1000, y2], [x, y], 50);
+    ctx.fillStyle = "black";
+    
+    ctx.fillStyle = "black";
+    if (hit2) {
+        ctx.fillStyle = "orange";
+        console.log("Animal is close . . .");
+    }
+
+    if (hit1) {
+        ctx.fillStyle = "red";
+        console.log("Animal is in danger zone ! ! !");
+    }
+    
+    ctx.beginPath();
+    ctx.arc(x, y, 50, 0, 2 * Math.PI);
+    if (hit1 || hit2) {
+        ctx.fill();
+    }
+    else {
+        ctx.stroke();
+    }
+}
+
+
+window.addEventListener('mousemove', mousemove);
---
+++ map/script.js
@@ -0,0 +1,30 @@
+var long = 14.469006330685156;
+var lat = 46.05076627820738;
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } 
+else {
+    console.log("Geolocation is not supported . . .");
+}
+
+
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude +
+    "\nLongitude: " + position.coords.longitude);
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+    loadMap();
+}
+
+function loadMap() {
+    // load map and set it to FRI location
+    var map = L.map('map').setView([lat, long], 15);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // create a marker for FRI
+    var markerFri = L.marker([46.05076627820738, 14.469006330685156]).addTo(map);
+    markerFri.bindPopup("Fakulteta za računalništvo in informatiko").openPopup();
+}
