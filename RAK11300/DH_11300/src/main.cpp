51f6e83acf88ceb83d0725735599cb7e94d0aa61
Luka Bergant <luka@bergant.com>
2022-05-14 12:47:57 +0200

    Added includes, configured PlatformIO

---
+++ RAK11300/DH_11300/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include "LoRaWan-Arduino.h" //http://librarymanager/All#SX126x
+#include <SPI.h>
+
+#include <stdio.h>
+
+#include "mbed.h"
+#include "rtos.h"
+
+#include <TinyGPS.h>
+
+using namespace std::chrono_literals;
+using namespace std::chrono;
+
+TinyGPS gps;
+
+// ************************************ IDs ************************************
+#include "keys.h"
+// ************************************     ************************************
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
