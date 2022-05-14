6b028ceb04998a516a73f7b22abb8f8aa532c5d5
Peter <ph2355@student.uni-lj.si>
2022-05-14 22:58:48 +0200

    server pobere podatke iz ttn in servira v /trackers

--- Web/backend/server.js
+++ Web/backend/server.js
@@ -1,13 +1,20 @@
+const { json } = require('express');
 const express = require('express');
+const https = require('node:https');
 const app = express();
+const querystring = require('querystring');
+require('dotenv').config();
 const port = 3000;
-let cors = require("cors");
+let cors = require('cors');
 app.use(cors());
 
 //const hardcoded_trackers = [{ latitude: 46.04975, longitude: 14.46907 }];
 const lat = 46.04975;
 const long = 14.46907;
 
+const API_KEY = process.env.API_KEY;
+
+const hardcoded_trackers = [{ latitude: 46.04975, longitude: 14.46907 }];
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
@@ -17,8 +24,42 @@ app.get('/trackers', (req, res) => {
 	let x = lat + Math.random() * (Math.round(Math.random()) ? 1 : -1) * 0.001;
 	let y = long + Math.random() * (Math.round(Math.random()) ? 1 : -1) * 0.001;
 	let coordinates = [{ latitude: x, longitude: y }];
-	res.json(coordinates);
-	//res.json(hardcoded_trackers);
+
+	const request = https.get(
+		'https://eu1.cloud.thethings.network/api/v3/applications/test-medo-application/devices?field_mask=locations',
+		{
+			headers: {
+				Authorization: `Bearer ${API_KEY}`,
+				Accept: 'application/json',
+				'User-Agent': 'nodeApp'
+			}
+		},
+		(response) => {
+			let data = '';
+
+			response.on('data', (chunk) => {
+				data += chunk;
+			});
+
+			response.on('end', () => {
+				console.log(data);
+				let obj = JSON.parse(data);
+				let locs = obj.end_devices.map((x) => {
+					return {
+						device_id: x.ids.device_id,
+						latitude: x.locations['frm-payload'].latitude,
+						longitude: x.locations['frm-payload'].longitude
+					};
+				});
+				res.json(locs);
+			});
+		}
+	);
+	request.on('error', (error) => {
+		console.log(error);
+		res.status(400);
+		res.end;
+	});
 });
 
 app.listen(port, () => {
