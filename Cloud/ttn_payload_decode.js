c8cd86b8af6e97e2c652f7b9097d9b9605d894d8
Peter <ph2355@student.uni-lj.si>
2022-05-15 03:03:32 +0200

    buton

--- Cloud/ttn_payload_decode.js
+++ Cloud/ttn_payload_decode.js
@@ -1,21 +1,34 @@
 function decodeUplink(input) {
-	return {
-		data: {
-			// bytes: input.bytes,
-			latitude:
-				(((input.bytes[1] << 24) >>> 0) +
-					((input.bytes[2] << 16) >>> 0) +
-					((input.bytes[3] << 8) >>> 0) +
-					(input.bytes[4] >>> 0)) /
-				100000.0,
-			longitude:
-				(((input.bytes[6] << 24) >>> 0) +
-					((input.bytes[7] << 16) >>> 0) +
-					((input.bytes[8] << 8) >>> 0) +
-					(input.bytes[9] >>> 0)) /
-				100000.0,
-			altitude: input.bytes[11] / 100.0,
-			hdop: 1.5
-		}
-	};
+	if (input.bytes[0] === 255) {
+		return {
+			data: {
+				latitude:
+					(((input.bytes[1] << 24) >>> 0) +
+						((input.bytes[2] << 16) >>> 0) +
+						((input.bytes[3] << 8) >>> 0) +
+						(input.bytes[4] >>> 0)) /
+					100000.0,
+				longitude:
+					(((input.bytes[6] << 24) >>> 0) +
+						((input.bytes[7] << 16) >>> 0) +
+						((input.bytes[8] << 8) >>> 0) +
+						(input.bytes[9] >>> 0)) /
+					100000.0,
+				altitude:
+					(((input.bytes[11] << 24) >>> 0) +
+						((input.bytes[12] << 16) >>> 0) +
+						((input.bytes[13] << 8) >>> 0) +
+						(input.bytes[14] >>> 0)) /
+					100.0,
+				hdop:
+					(((input.bytes[15] << 24) >>> 0) +
+						((input.bytes[16] << 16) >>> 0) +
+						((input.bytes[17] << 8) >>> 0) +
+						(input.bytes[18] >>> 0)) /
+					10000.0
+			}
+		};
+	} else {
+		return {};
+	}
 }
--- map/script.js
+++ map/script.js
@@ -3,73 +3,96 @@ var long = 14.469006330685156;
 var lat = 46.05076627820738;
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-} 
-else {
-    console.log("Geolocation is not supported . . .");
+	navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+	console.log('Geolocation is not supported . . .');
 }
 
-var getJSON = function(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
-    };
-    xhr.send();
+var getJSON = function (url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+	xhr.responseType = 'json';
+	xhr.onload = function () {
+		var status = xhr.status;
+		if (status === 200) {
+			callback(null, xhr.response);
+		} else {
+			callback(status, xhr.response);
+		}
+	};
+	xhr.send();
 };
 
-
 async function showPosition(position) {
-    console.log("Latitude: " + position.coords.latitude +
-    "\nLongitude: " + position.coords.longitude);
-    long = position.coords.longitude;
-    lat = position.coords.latitude;
-    loadMap();
+	console.log(
+		'Latitude: ' +
+			position.coords.latitude +
+			'\nLongitude: ' +
+			position.coords.longitude
+	);
+	long = position.coords.longitude;
+	lat = position.coords.latitude;
+	loadMap();
 }
+
+function pantotracker() {
+	console.log('hello');
+}
+
+pantotracker();
 
 function loadMap() {
-    // load map and set it to current location
-    var map = L.map('map').setView([lat, long], 15);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+	// load map and set it to current location
+	var map = L.map('map').setView([lat, long], 15);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution:
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
 
-    // create a marker for FRI
-    var markerFri = L.marker([46.05076627820738, 14.469006330685156]).addTo(map);
-    markerFri.bindPopup("Fakulteta za računalništvo in informatiko").openPopup();
+	// create a marker for FRI
+	var markerFri = L.marker([46.05076627820738, 14.469006330685156], {
+		riseOnHover: true
+	}).addTo(map);
+	markerFri
+		.bindPopup('Fakulteta za računalništvo in informatiko')
+		.openPopup();
 
-    var markers = new Array;
+	var markers = new Array();
 
-    window.setInterval(function() {
-        getJSON('http://localhost:3000/trackers',
-        function(err, data) {
-            if (err !== null) {
-                alert('Something went wrong: ' + err);
-            } 
-            else {
-                if (markers.length >= 1) {
-                    for (let i = 0; i < data.length; i++) {
-                        markers.shift().remove();
-                    }
-                }
-                for (let i = 0; i < data.length; i++){
-                    console.log(data[i].longitude, data[i].latitude, data[i].device_id)
-                    var marker = L.marker([data[i].latitude, data[i].longitude]).addTo(map);
-                    marker.bindPopup(data[i].device_id);
-                    markers.push(marker);
-                }
-            }
-        });
-      }, 5000);
-    
+	window.setInterval(function () {
+		getJSON('http://localhost:3000/trackers', function (err, data) {
+			let trackerDiv = '';
+			if (err !== null) {
+				alert('Something went wrong: ' + err);
+			} else {
+				if (markers.length >= 1) {
+					for (let i = 0; i < data.length; i++) {
+						markers.shift().remove();
+					}
+				}
+				for (let i = 0; i < data.length; i++) {
+					console.log(
+						data[i].longitude,
+						data[i].latitude,
+						data[i].device_id
+					);
+					var marker = L.marker(
+						[data[i].latitude, data[i].longitude],
+						{
+							riseOnHover: true
+						}
+					).addTo(map);
+					marker.bindPopup(data[i].device_id);
+					markers.push(marker);
+
+					trackerDiv +=
+						'<div class="tracker" onclick="pantotracker()"><p>' +
+						data[i].device_id +
+						'</p></div>';
+				}
+
+				document.getElementById('toolbar').innerHTML = trackerDiv;
+			}
+		});
+	}, 5000);
 }
-
-
-
-
